5a9659bdc775310eb0640477ce031f00f0f59bfc
Aprilwairimu <april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.com>
2022-04-23 12:53:32 +0300

    added a user and credential class

---
+++ credentials.py
@@ -0,0 +1,11 @@
+class Credentials:
+    """"
+    Class that generates credential for the user
+    """
+    Credentials_list = []
+
+    def __init__(self,app_name,user_name,password):
+        self.app_name =app_name
+        self.app_name =user_name
+        self.app_name =password
+            
